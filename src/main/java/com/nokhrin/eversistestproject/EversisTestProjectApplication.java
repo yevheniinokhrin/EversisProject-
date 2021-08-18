e3baeb0a6f09db23c80a0c4088d68f3db0a711d9
yevheniinokhrin <233315@edu.p.lodz.pl>
2021-08-18 20:46:46 +0200

    first commit

---
+++ src/main/java/com/nokhrin/eversistestproject/EversisTestProjectApplication.java
@@ -0,0 +1,13 @@
+package com.nokhrin.eversistestproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EversisTestProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EversisTestProjectApplication.class, args);
+    }
+
+}
